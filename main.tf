ddc020c9305ce582126052f33f36d900f741ec1d
rajan612 <rajan612@gmail.com>
2024-01-16 17:58:39 -0500

    added sg group

--- main.tf
+++ main.tf
@@ -15,9 +15,50 @@ module "vpc" {
     terraform   = "true"
     Environment = "dev"
   }
+
+  public_subnet_tags = {
+    Name = "jenkins-subnet"
+  }
 }
 
 
 #SG
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "Jenkins-SG"
+  description = "SG for Jenkins"
+  vpc_id      = module.vpc.default_vpc_id
+
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "ssh"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  tags = {
+    Name = "Jenkins-SG"
+  }
+}
+
 
 #EC2 instance
