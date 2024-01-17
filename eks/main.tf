12efdb4b26b9d1894900a21c270456be2939e4e3
rajan612 <rajan612@gmail.com>
2024-01-16 22:01:14 -0500

    typo correction

--- eks/main.tf
+++ eks/main.tf
@@ -6,7 +6,7 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.azs.names
-  private_subnets = var.public_subnets
+  private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
   enable_dns_hostnames = true
